ef42f5b2b842fbe4367156ed69a6fd93e8068145
mohamed-sabry-1789 <153429746+mohamed-sabry-1789@users.noreply.github.com>
2024-05-11 15:28:26 +0300

    updet-working objects

--- guess.js
+++ guess.js
@@ -26,13 +26,73 @@ const gameContext = {
     numberOfHint: 2,
     restHint() { this.numberOfHint = 2 },
     nextHint() { this.numberOfHint-- },
-    hintIndex: [],
     currentTry: 1,
     curentTryF() { return this.currentTry },
     nextCurrentTry() { this.currentTry++ },
     restCurrentTry() { this.currentTry = 1 }
 }
-let massageArea = document.querySelector(".massage")
+// let massageArea = document.querySelector(".massage")
+function isLetter(letter) {
+    return letter.match(/^[a-z]{1}$/i)
+}
+const gameUi = {
+    // Controls
+    keyColor: document.querySelector(".key-colors"),
+    messageArea: document.querySelector(".massage"),
+    checkButton: document.querySelector(".check"),
+    hintButton: document.querySelector(".hint"),
+    hintSpan: document.querySelector(".hint span"),
+
+    // toggleKeyColor(true) -> show -> "block"
+    // toggleKeyColor(false) -> hide -> "none"
+
+    // toggleKeyColor("none")
+    // toggleKeyColor("block")
+
+    // Functions
+    toggleKeyColor(show) { this.keyColor.style.display = show ? "block" : "none" },
+    toggleCheckButton(enable) { this.checkButton.disabled = enable },
+    toggleHintButton(enable) { this.hintButton.disabled = enable },
+    setHint(count) { this.hintSpan.innerHTML = count },
+    setMessage(word, messageBody) {
+        if (word && messageBody) {
+            const wordWrapper = document.createElement("span")
+            const wordNode = document.createTextNode(word)
+            wordWrapper.appendChild(wordNode)
+            const bodyText = document.createTextNode(messageBody)
+            this.messageArea.append(bodyText, wordWrapper)
+        } else {
+            this.messageArea.innerHTML = " "
+        }
+    },
+    win(word) {
+        this.toggleKeyColor(false)
+        this.toggleCheckButton(true);
+        this.toggleHintButton(true);
+        this.setMessage(word, "you win the word is")
+        // let span = document.createElement("span")
+        // let text = document.createTextNode(`${word}`)
+        // let text2 = document.createTextNode("you win the word is")
+        // span.append(text)
+        // this.messageArea.append(text2, span)
+    },
+    gameOver(word) {
+        this.toggleKeyColor()
+        this.messageArea.classList.add("gameOver")
+        // const span = document.createElement("span")
+        // const text = document.createTextNode(`${word}`)
+        // span.appendChild(text)
+        // const text2 = document.createTextNode(`game over the word is`)
+        // this.messageArea.append(text2, span)
+        this.setMessage(word, "game over the word is")
+        this.toggleCheckButton(true);
+        this.toggleHintButton(true);
+    }
+}
+
+// this.hintButton.innerHTML = --
+// gameUi.toggleCheckButton(true)
+// document.querySelector(".check-btn").disabled = true    !!same result
 
 function addUpperCaseAndFoucinInput(input) {
     // input -> value
@@ -42,7 +102,17 @@ function addUpperCaseAndFoucinInput(input) {
     input.addEventListener("keydown", function (event) {
         // console.log({ event })
         const key = event.key;
-        if (key.match(/^[a-z]{1}$/i) && !(event.altKey || event.ctrlKey || event.metaKey)) {
+
+        if (key === " ") {
+            if (input.nextElementSibling) {
+                input.nextElementSibling.focus();
+            } else {
+                input.parentElement.children[1].focus()
+            }
+
+
+        }
+        if (isLetter(key) && !(event.altKey || event.ctrlKey || event.metaKey)) {
             event.preventDefault();
             this.value = key.toUpperCase();
             if (input.nextElementSibling) {
@@ -53,31 +123,40 @@ function addUpperCaseAndFoucinInput(input) {
 
     })
 }
+const logic = {
+    logic(input) {
+        if (input.nextElementSibling) {
+            input.nextElementSibling.focus();
+        } else {
+            input.parentElement.children[1].focus()
+        }
+    },
+    btn() {
+        return document.querySelector("button")
+    }
+}
+
 function addarrwInput(input) {
     input.addEventListener("keydown", function (event) {
         if (event.key === "ArrowRight") {
-            if (input.nextElementSibling) {
-                input.nextElementSibling.focus();
-            } else {
-                input.parentElement.children[1].focus()
-            }
-
-
+            logic.logic(input)
         }
 
         if (event.key === "ArrowLeft") {
             const prevInput = input.previousElementSibling;
+            // if (prevInput && prevInput.tagName === 'INPUT')
             if (isInputElement(prevInput)) {
 
-                if (prevInput.classList.contains("hintInput")) {
+                if (checkInput(prevInput)) {
                     const prevPrevInput = prevInput.previousElementSibling;
                     if (isInputElement(prevPrevInput)) {
-                        if (prevPrevInput.classList.contains("hintInput")) {
+                        if (checkInput(prevPrevInput)) {
                             const prevPrevPrevInput = prevPrevInput.previousElementSibling;
                             if (isInputElement(prevPrevPrevInput)) {
                                 prevPrevPrevInput.focus();
                             } else {
                                 input.parentElement.lastElementChild.focus()
+
                             }
                         } else {
                             prevPrevInput.focus();
@@ -105,7 +184,7 @@ function isInputElement(element) {
 
 function inputLisner(input) {
     input.addEventListener("focus", () => {
-        if (input.classList.contains("hintInput")) {
+        if (checkInput(input)) {
             if (input.nextElementSibling) {
                 input.nextElementSibling.focus()
             } else {
@@ -115,18 +194,21 @@ function inputLisner(input) {
 
     })
 }
+function checkInput(input) {
+    return input.classList.contains("hintInput") || input.classList.contains("yes-in-place")
+
+}
 function deleteLetterinInput(input) {
     input.addEventListener("keydown", (event) => {
         if (event.key === "Backspace") {
             if (input.value) {
-                if (input.classList.contains("hintInput")) {
+                if (checkInput(input)) {
                     event.preventDefault();
                 } else {
                     input.value = ""
                 }
             } else if (input.previousElementSibling) {
-                if (!input.previousElementSibling.classList.contains("hintInput")) {
-
+                if (!checkInput(input.previousElementSibling)) {
                     input.previousElementSibling.value = ""
                     input.previousElementSibling.focus();
                 } else {
@@ -181,23 +263,23 @@ const checkButton = document.querySelector(".check")
 
 
 // console.log(wordGuess)
-function guessCheck(context) {
+function guessCheck({ context, ui }) {
     const numbersOfInputs = context.wordLength();
     const wordGuess = context.currentWord();
-
     // const curentTry = context.curentTryF();
-    const curentTry = context.curentTry;
+    // const curentTry = context.curentTry;
     let successguess = true;
     for (let i = 1; i <= numbersOfInputs; i++) {
         const inputField = document.querySelector(`#guess-${context.currentTry}-input-${i}`)
-
         const Letter = inputField.value.toLowerCase();
-
         const atctualLetter = wordGuess[i - 1]
 
         if (Letter === atctualLetter) {
             //letter is correct and in place
             inputField.classList.add("yes-in-place")
+
+
+
             //letter is correct and not in place
         } else if (wordGuess.includes(Letter) && Letter !== "") {
             inputField.classList.add("not-in-place")
@@ -213,24 +295,28 @@ function guessCheck(context) {
             inputField.classList.add("no")
             successguess = false;
         }
+
     }
     //check if user win or lose
     if (successguess) {
-        let span = document.createElement("span")
-        let text = document.createTextNode(`${wordGuess}`)
-        let text2 = document.createTextNode("you win the word is")
-        span.append(text)
-        massageArea.append(text2, span)
-        const a = document.querySelector(".key-colors")
-        a.style.display = "none"
+        ui.win(wordGuess);
+        // let span = document.createElement("span")
+        // let text = document.createTextNode(`${wordGuess}`)
+        // let text2 = document.createTextNode("you win the word is")
+        // span.append(text)
+        // massageArea.append(text2, span)
+        // const a = document.querySelector(".key-colors")
+        // a.style.display = "none"
         //////add disabled class on ALL try Divs 
         let allTries = document.querySelectorAll('.try');
         allTries.forEach((tryDiv) => {
             tryDiv.classList.add("disabled-inputs")
         })
         // disabled check button and hint button
-        checkButton.disabled = true;
-        hintButton.disabled = true;
+        // checkButton.disabled = true;
+        // hintButton.disabled = true;
+        // ui.toggleCheckButton(true);
+        // ui.toggleHintButton(true);
 
     } else {
         // disabled curentTry 
@@ -239,9 +325,10 @@ function guessCheck(context) {
         const currentTryInputs = document.querySelectorAll(`.try-${context.currentTry} > input`)
         currentTryInputs.forEach((input => {
             input.disabled = true;
-        }))
 
+        }))
         context.nextCurrentTry()
+
 
         const el = document.querySelector(`.try-${context.currentTry}`)
         if (el) {
@@ -249,24 +336,43 @@ function guessCheck(context) {
             el.classList.remove("disabled-inputs")
             // Undisabled  next curentTry  input
             const nextTryInputs = document.querySelectorAll(`.try-${context.currentTry} > input`)
-
-            nextTryInputs.forEach((input) => {
+            for (let i = 0; i < nextTryInputs.length; i++) {
+                let input = nextTryInputs[i];
                 input.disabled = false;
-            })
+                const prevInputField = document.querySelector(`#guess-${context.currentTry - 1}-input-${i + 1}`)
+
+                console.log(prevInputField, i, context.currentTry)
+
+                const prevLetter = prevInputField.value.toLowerCase();
+                const atctualLetter = wordGuess[i]
+                console.log(wordGuess)
+                if (prevLetter === atctualLetter) {
+                    //letter is correct and in place
+                    input.classList.add("yes-in-place")
+                    input.value = atctualLetter.toUpperCase()
+                    //letter is correct and not in place
+
+                }
+            }
+
             el.children[1].focus();
         } else {
-            massageArea.classList.add("gameOver")
-            const span = document.createElement("span")
-            const text = document.createTextNode(`${wordGuess}`)
-            span.appendChild(text)
-            const text2 = document.createTextNode(`game over the word is`)
-            massageArea.append(text2, span)
+            ui.gameOver(wordGuess);
+            // massageArea.classList.add("gameOver")
+            // const span = document.createElement("span")
+            // const text = document.createTextNode(`${wordGuess}`)
+            // span.appendChild(text)
+            // const text2 = document.createTextNode(`game over the word is`)
+            // massageArea.append(text2, span)
             //disabled check button  and hint button
-            checkButton.disabled = true;
-            hintButton.disabled = true;
-            const a = document.querySelector(".key-colors")
-            a.style.display = "none"
+            // checkButton.disabled = true;
+            // hintButton.disabled = true;
+            // ui.toggleCheckButton(true);
+            // ui.toggleHintButton(true);
+            // const a = document.querySelector(".key-colors")
+            // a.style.display = "none"
         }
+
     }
 }
 
@@ -275,7 +381,9 @@ function guessCheck(context) {
 document.querySelector(".hint span").innerHTML = gameContext.numberOfHint
 const hintButton = document.querySelector(".hint")
 
-function hint(context) {
+function hint({ context, ui }) {
+    // const {context , ui} = args ?? {}
+    if (!context || !ui) return
     // get enabled input from input 
     let enabledInput = document.querySelectorAll("input:not([disabled])")
     const wordGuess = context.currentWord();
@@ -305,10 +413,11 @@ function hint(context) {
             }
             if (context.numberOfHint > 0) {
                 context.nextHint();
-                document.querySelector(".hint span").innerHTML = context.numberOfHint
-
+                // document.querySelector(".hint span").innerHTML = context.numberOfHint
+                ui.setHint(context.numberOfHint)
                 if (context.numberOfHint === 0) {
-                    hintButton.disabled = true;
+                    // hintButton.disabled = true;
+                    ui.toggleHintButton(true)
 
                 }
             }
@@ -328,34 +437,42 @@ function ent(e) {
 const rest = document.querySelector(".rest")
 
 
-function removeInputs(context) {
+// gameUi = {
+//     inputs: document.querySelecotr(".inputs"),
+//     // functions
+// }
+function removeInputs({ context, ui }) {
     const tryDiv = document.querySelector(".inputs")
     const tryDivs = document.querySelectorAll(".inputs > div")
-    tryDivs.forEach((li) => {
-        if (li.parentElement) {
-            li.remove()
+    tryDivs.forEach((el) => {
+        if (el.parentElement) {
+            el.remove()
         }
     })
     context.reset();
     genrateInput(context)
     hintButton.disabled = false
     checkButton.disabled = false
-    document.querySelector(".massage").innerHTML = " "
-    document.querySelector(".key-colors").style.display = "block"
+    // document.querySelector(".massage").innerHTML = " "
+    // ui.messageArea.innerHTML = " "
+    ui.setMessage()
+    // document.querySelector(".key-colors").style.display = "block"
+    ui.toggleKeyColor(true)
     context.restHint()
     context.restCurrentTry();
-    document.querySelector(".hint span").innerHTML = context.numberOfHint;
+    // document.querySelector(".hint span").innerHTML = context.numberOfHint;
+    ui.setHint(context.numberOfHint);
 }
 
-
-rest.addEventListener("click", () => { removeInputs(gameContext) })
+rest.addEventListener("click", () => { removeInputs({ context: gameContext, ui: gameUi }) })
 // rest.addEventListener("click", () => {
 //     window.location.reload()
 // })
 document.addEventListener("keydown", ent)
-hintButton.addEventListener("click", () => { hint(gameContext) })
+hintButton.addEventListener("click", () => { hint({ context: gameContext, ui: gameUi }) })
 
-checkButton.addEventListener("click", () => { guessCheck(gameContext) })
+checkButton.addEventListener("click", () => { guessCheck({ context: gameContext, ui: gameUi }) })
 
 window.onload = genrateInput(gameContext);
+
 
---
+++ test.js
@@ -0,0 +1,133 @@
+// const ser = {
+//     name: "ahmed",
+//     age: 35,
+//     city: "alex",
+//     num() { return 10 }
+// }
+// const sr = {
+//     name: "nabil",
+//     age: 50,
+//     city: "cairo",
+//     num() { return 9 }
+// }
+
+
+
+// // function test() {
+// //     console.log(t)
+// //     console.log(r)
+// //     console.log(d)
+// //     console.log(c)
+
+// // }
+
+// // test()
+
+// // test({
+// //     name: "mohamed",
+// //     age: 242,
+// //     city: "alex",
+// //     num() { return 10 }
+// // })
+// // test()
+// test({ mo: ser, o: sr })
+// function test({ mo, s, o }) {
+//     console.log(mo)
+//     console.log(o)
+
+// }
+// // test(ser, sr)
+// // function test(mo, o) {
+// //     console.log(mo)
+// //     console.log(o)
+
+
+// // }
+
+
+// // const user = {
+
+// //     theAge: 39,
+// //     skills: ["html", "css", "js"],
+// //     adress: {
+
+// //         ksa: "riad"
+// //     }
+// // }
+
+// // const { theName = "karim", theAge, skills: [html, css], adress: { egypt = "alex", ksa, anglen = "londo" } } = user
+
+// // console.log(theName)
+// // console.log(theAge)
+// // console.log(html)
+// // console.log(egypt)
+// // console.log(anglen)
+
+
+// // const fn = (name, age, gender) => {
+// //     let message = "";
+// //     if (gender === 'm') {
+// //         message += 'Mr '
+// //     } else {
+// //         message += 'Ms '
+// //     }
+// //     if (age) {
+// //         message += `${age} years old `
+// //     }
+// //     message += name;
+// //     return message
+// // }
+// // fn("Sabry", 20, "f"); // Ms 20 years old Sabry
+// // fn("Sabry", undefined, "m") // Mr Sabry
+
+
+// // const fn2 = ({ person, admin }) => {
+// //     const { name, age, gender } = person
+// //     let message = "";
+// //     if (gender === 'm') {
+// //         message += 'Mr '
+// //     } else {
+// //         message += 'Ms '
+// //     }
+// //     if (age) {
+// //         message += `${age} years old `
+// //     }
+// //     message += name;
+// //     if (admin) {
+// //         message += " is an Admin."
+// //     }
+// //     return message
+// // }
+// // const sandy = {
+// //     name: "Sabry",
+// //     gender: "f",
+// //     age: 20,
+// // }
+// // fn2({ person: sandy, admin: true });
+// // const sabry = {
+// //     name: "Sabry",
+// //     gender: "m",
+
+// // }
+// // fn2({ person: sabry, admin: false });
+
+
+
+// // let user = {
+// //     name: "kaml",
+// //     skills: ['css']
+// // }
+
+
+// // // let nameU = user.name
+// // let skills = user.skills
+
+// // // console.log(nameU)
+
+// // console.log(skills)
+// // user.skills.push("html")
+// // console.log(skills)
+
+
+
+
